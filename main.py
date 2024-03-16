febe6ce129f52aa1aa4835f7a51a9c80ace24964
Space Sheperd <96922638+spacesheperd@users.noreply.github.com>
2024-03-16 12:03:55 +0100

    Update main.py

--- main.py
+++ main.py
@@ -5,14 +5,6 @@ import subprocess
 import sys
 import time
 
-    
-#conso des différents profils MSI AfterBurner en VA
-profil1=240
-profil2=310
-profil3=425
-profil4=480
-profil5=100
-
 #initialisation nombre de profils utilisés 
 nombre_profil1 = 0
 nombre_profil2 = 0
@@ -20,7 +12,10 @@ nombre_profil3 = 0
 nombre_profil4 = 0
 nombre_profil5 = 0
 
-#definition de l'affichage
+#définition du nombre de secondes avant de relancer le programme
+temps = 60
+
+#definition de l'affichage pour savoir combien de fois les différents profils ont été utilisés
 def affichage():
     print("Le profil 1 a été utilisé ", nombre_profil1, "de fois.")
     print("Le profil 2 a été utilisé ", nombre_profil2, "de fois.")
@@ -31,7 +26,7 @@ def affichage():
 #initialise conso_carte pour la première fois
 conso_carte=0
 
-#définit la consomation des différents profils
+#initialise la consomation des différents profils
 conso_profil1=0
 conso_profil2=0
 conso_profil3=0
@@ -40,7 +35,6 @@ conso_profil5=0
 
 # URL de l'API
 url = "https://envoy.local/production.json"
-
 
 token = get_token()
 while True:
@@ -55,20 +49,20 @@ while True:
         # Charger la réponse JSON
         data = response.json()
 
-        # Extraire le "wNow" de la deuxième entrée de la liste "consumption"
+        # Extraire le "wNow" de la deuxième entrée de la liste "consumption" (ce qui nous donne l'exportation d'électricité en temps réel)
         consumption_w_now = data.get("consumption", [])[1].get('wNow', 'N/A')
 
         #retournerment de variables pour que ça soit positif car flemme de faire des calculs :)
-        exporté = consumption_w_now * -1
-        print("consommation actuelle :", exporté)
+        exporté = consumption_w_now * -1 #donc maintenant si c'est positif ça veut dire qu'il y a exportation d'électricité, si c'est négatif alors c'est importation d'électricité
+        print("consommation actuelle :", exporté) #pour check la véritable exportation avant d'y toucher
 
-        #sélectionne la marge de sécurité de consommation en Watt
+        #sélectionne la marge de sécurité de consommation en Watt (au cas où il y a un pic d'électricité dans la maison)
         marge=200
 
-        #calcul pour combien ça consomme
+        #calcul pour combien ça consomme en prennant en compte la marge et la consommation de la carte graphique (carte graphique ou du système)
         energie = exporté - marge + conso_carte
 
-        #profils 
+        #profils MSI afterburner à paramétrer comme on le souhaite avant
         if energie <=150:
             nombre_profil5 = nombre_profil5 + 1
             affichage()
@@ -103,11 +97,11 @@ while True:
             print(energie, "Profil 4 actuellement activé")
             subprocess.run('"C:\Program Files (x86)\MSI Afterburner\MSIAfterburner.exe" -profile4', shell=True) #lance le profil 4 (état maximal)
             conso_carte=370
-
+    #avoir un retour détaillé sur les erreurs
     except requests.RequestException as e:
         print(f"Une erreur s'est produite lors de la requête HTTP : {e}")
     except json.JSONDecodeError as e:
         print(f"Une erreur s'est produite lors du décodage JSON : {e}")
     except Exception as e:
         print(f"Une erreur s'est produite : {e}")
-    time.sleep(60)
+    time.sleep(temps) #relancer le programme toutes les X secondes
