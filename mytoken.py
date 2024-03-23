098449a8217fa487fad10582245c4ac802f3fd81
Space Sheperd <96922638+spacesheperd@users.noreply.github.com>
2024-03-23 09:17:10 +0100

    Update mytoken.py
    ajout du .json

--- mytoken.py
+++ mytoken.py
@@ -1,11 +1,7 @@
 import json
 import requests
 
-user = ''
-password = ''
-envoy_serial = ''
-
-def get_token():
+def get_token(user, password, envoy_serial):
     # Authentification
     data = {'user[email]': user, 'user[password]': password}
     response = requests.post('http://enlighten.enphaseenergy.com/login/login.json?', data=data)
@@ -15,14 +11,14 @@ def get_token():
 
         if 'session_id' in response_data:
             # Obtenir le jeton
-            data = {'session_id': response_data['session_id'], 'serial_num': envoy_serial, 'username': user}
-            response = requests.post('http://entrez.enphaseenergy.com/tokens', json=data)
+            token_data = {'session_id': response_data['session_id'], 'serial_num': envoy_serial, 'username': user}
+            token_response = requests.post('http://entrez.enphaseenergy.com/tokens', json=token_data)
 
-            if response.status_code == 200:
-                print(response.text)
-                return response.text
+            if token_response.status_code == 200:
+                print(token_response.text)
+                return token_response.text
             else:
-                print(f"La requête pour obtenir le jeton a échoué avec le code d'état {response.status_code}.")
+                print(f"La requête pour obtenir le jeton a échoué avec le code d'état {token_response.status_code}.")
         else:
             print("La clé 'session_id' est manquante dans response_data.")
     else:
@@ -31,5 +27,15 @@ def get_token():
         else:
             print(f"La requête a échoué avec le code d'état {response.status_code}.")
 
+# Lecture des informations du fichier JSON
+with open("C:/Users/mathi/Desktop/visualstudiocode/minage_script/infos.json", "r") as f:
+    json_data = json.load(f)
+
+# Récupération des informations nécessaires
+credentials = json_data[0]  # Le premier objet JSON
+user = credentials['username']
+password = credentials['password']
+envoy_serial = credentials['envoy_serial']
+
 # Appel de la fonction pour obtenir le jeton
-get_token()
+get_token(user, password, envoy_serial)
